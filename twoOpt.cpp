a574b20ecc104a360c6a0c1a0e9da71c17e53ea5
Catalina-ui <58471544+Catalina-ui@users.noreply.github.com>
2020-01-17 14:16:46 +0900

    Create twoOpt.cpp

---
+++ twoOpt.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> twoOpt(vector<vector<int>> dis,vector<int> route){
+    for (int i = 0; i < (int)route.size()-1; i++)
+    {
+        int vMax = 0, iMax = route.size() + 1;
+
+        for (int j = i + 2; j < N;j++){
+            int tmp = dis[route[i]][route[i + 1]] + dis[route[j]][route[j + 1]] - dis[route[i]][route[j]] - dis[route[i + 1]][route[j + 1]];
+            if(vMax < tmp && route[i] != route[j+1]){
+                vMax = tmp;
+                iMax = j;
+            }
+        }
+
+        if (vMax != 0)
+        {
+            int r = i + 1, l = iMax;
+            while(l - r > 0){
+                swap(route[r], route[l]);
+                r++;
+                l--;
+            }
+        }
+    }
+    return route;
+}
