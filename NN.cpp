0725dd71d7866a7de5e59e580497060601493e7e
Catalina-ui <58471544+Catalina-ui@users.noreply.github.com>
2020-01-27 21:46:25 +0900

    Update NN.cpp

--- NN.cpp
+++ NN.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <vector>
-using namespace std;
 
-vector<int> NN(vector<vector<int>> dis,int pos){
-    int dis_size = dis.size();
-    vector<int> route(dis_size + 1,pos);
-    vector<bool> visited(dis_size, false);
+vector<int> NN(int pos)
+{
+    vector<int> route(N, pos);
+    vector<bool> visited(N, false);
     visited[pos] = true;
     int now = 1;
-    while(now <= dis_size){
-        int vMin = 2147483647,iMin = dis_size + 1;
-        for (int i = 0; i < dis_size;i++){
-            if(!visited[i] && vMin > dis[route[now-1]][i]){
+    while (now < N)
+    {
+        int vMin = 2147483647, iMin = N + 1;
+        for (int i = 0; i < N; i++)
+        {
+            if (!visited[i] && vMin > dis[route[now - 1]][i])
+            {
                 vMin = dis[route[now - 1]][i];
                 iMin = i;
             }
